c69ad8f6dc0d8eeb3faf86a6792a0a5a7f36123e
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-08-16 19:03:19 +0530

    adding log in page and databse

--- ClickCharm/shop/urls.py
+++ ClickCharm/shop/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
    path("", views.index, name="shophome"),
+   path("About/", views.index, name="AboutUs"),
+   path("Contact/", views.contact, name="ContactUs"),
+   path("Traker/", views.tracker, name="TrackingStatus"),
+   path("search/", views.search, name="Search"),
+   path("productview/", views.productview, name="Productview"),
+   path("checkout/", views.checkout, name="Checkout")
 ]
--- ClickCharm/shop/views.py
+++ ClickCharm/shop/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'shop/index.html')
+def contact(request):
+    return render(request, 'shop/index2.html')
+def tracker(request):
+    return render(request, 'shop/index.html')
+def search(request):
+    return render(request, 'shop/index.html')
+def productview(request):
+    return render(request, 'shop/index.html')
+def checkout(request):
+    return render(request, 'shop/index.html')
