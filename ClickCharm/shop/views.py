abe925a29a7ba40d7556a3d12121fb9c9cb97fbd
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-08-24 15:04:05 +0530

    added photos and slider

--- ClickCharm/shop/views.py
+++ ClickCharm/shop/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import product  
+from math import ceil
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products= product.objects.all()
+    n= len(products)
+    nSlides= n//4 + ceil((n/4) + (n//4))
+    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    return render(request,"shop/index.html", params)
 def about(request):
     return render(request, 'shop/about.html')
 def contact(request):
