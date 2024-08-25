438364839ca54d50286a8085e90fb0fb166fcc28
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-08-25 11:54:07 +0530

    category wise items

--- ClickCharm/shop/views.py
+++ ClickCharm/shop/views.py
@@ -6,11 +6,19 @@ from math import ceil
 
 # Create your views here.
 def index(request):
-    products= product.objects.all()
-    n= len(products)
-    nSlides= n//4 + ceil((n/4) + (n//4))
+    # products= product.objects.all()
+    # n= len(products)
+    # nSlides= n//4 + ceil((n/4) + (n//4))
+    allprods =[]
+    catprods = product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides= n//4 + ceil((n/4) + (n//4))
+        allprods.append([prod, range(1, nSlides), nSlides])
     # params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
-    allprods = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
+    # allprods = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
     params = {'allprods' : allprods }
     return render(request,"shop/index.html", params)
 def about(request):
