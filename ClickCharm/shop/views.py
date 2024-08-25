bcc27c915f3ad59ea3b483dbcb23a148ff409947
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-08-25 10:16:12 +0530

    added multiple slides

--- ClickCharm/shop/views.py
+++ ClickCharm/shop/views.py
@@ -9,7 +9,9 @@ def index(request):
     products= product.objects.all()
     n= len(products)
     nSlides= n//4 + ceil((n/4) + (n//4))
-    params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    # params={'no_of_slides':nSlides, 'range':range(1,nSlides), 'product': products}
+    allprods = [[products, range(1, nSlides), nSlides], [products, range(1, nSlides), nSlides]]
+    params = {'allprods' : allprods }
     return render(request,"shop/index.html", params)
 def about(request):
     return render(request, 'shop/about.html')
